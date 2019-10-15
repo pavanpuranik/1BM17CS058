30d83eda2c46a772da0241ff1d8493f5fbe71929
pavanpuranik <48992228+pavanpuranik@users.noreply.github.com>
2019-10-15 11:48:29 +0530

    Add files via upload

---
+++ lab7b.py
@@ -0,0 +1,10 @@
+def print_horiz_line():
+    print(" --- " * 3)
+
+def print_vert_line():
+    print("|   " * 4)
+
+for index in range(3):
+      print_horiz_line()
+      print_vert_line()
+print_horiz_line()
