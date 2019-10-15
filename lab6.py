68bf3a7aae014411b236a63d689e2f6f9c96fda6
pavanpuranik <48992228+pavanpuranik@users.noreply.github.com>
2019-10-15 11:37:51 +0530

    Add files via upload

---
+++ lab6.py
@@ -0,0 +1,51 @@
+def areParanthesisBalanced(expr) :  
+  
+    s = [];  
+  
+    for i in range(len(expr)) : 
+  
+        if (expr[i] == '(' or 
+            expr[i] == '[' or expr[i] == '{') : 
+  
+        
+            s.append(expr[i]);  
+            continue;  
+  
+        if (len(s) == 0) : 
+            return False;  
+  
+        if expr[i] == ')' : 
+  
+            x = s.pop(); 
+              
+            if (x == '{' or x == '[') : 
+                return False;  
+  
+        elif expr[i] == '}':  
+  
+            x = s.pop(); 
+              
+            if (x == '(' or x == '[') : 
+                return False;  
+          
+        elif x == ']':  
+  
+         
+            x = s.pop(); 
+              
+            if (x =='(' or x == '{') : 
+                return False;  
+  
+  
+    if len(s) : 
+        return True
+    else : 
+        return False
+
+expr = "{()}[]";  
+      
+if (areParanthesisBalanced(expr)) : 
+        print("Balanced");  
+else : 
+        print("Not Balanced");  
+  
